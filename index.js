90613142a0fa21e983e562867d01c9d3aa7047ba
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-05 04:49:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length(); i++) {
     for (var j = 0; j < instruments.length(); j ++) {
-      
+      array.push(musicians[i] + "plays" + instruments[j]);
     }
   }
+  return array;
 }
