17e36c3d47b62dcd3bb3bc685ab1a38d2a0fe7fd
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-05 04:57:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function johnLennonFacts(array) {
 function iLoveTheBeatles(n) {
   var array = [];
   do{
-    array.push("I love the Beatles!")
+    array.push("I love the Beatles!");
   } while (n > 0 && n < 15);
   return array;
 }
